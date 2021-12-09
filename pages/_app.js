7e902bb472a1061c8f3dd869f8a5b9c2bd8236ab
Cherise Stanley <61471777+catstacks@users.noreply.github.com>
2021-12-09 16:09:54 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,16 @@
-import Head from 'next/head'
+import '../styles/globals.css'
 import Nav from '../components/Nav'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>Next and MDX Blog</title>
-        <link href="<https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css>" rel="stylesheet"
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
               integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
               crossOrigin="anonymous" />
       </Head>
-
       <div className="container">
         <Nav />
         <main>
@@ -19,6 +19,6 @@ function MyApp({ Component, pageProps }) {
       </div>
     </>
   )
-}                                                                                                           
+}
 
 export default MyApp
