3a70304aa07472c4d2d3b93e3b59370bb0b3fcc2
Cherise Stanley <61471777+catstacks@users.noreply.github.com>
2021-12-09 16:19:23 +0000

    Update bio.js

--- pages/bio.js
+++ pages/bio.js
@@ -1,8 +1,8 @@
 const Bio = () => {
     return (
       <div className="mt-3">
-        <p className="display-4 text-center">I'm Cherise Stanley</p>
-        <p className="text-center">Cherise Stanley's bio here</p>
+        <p className="display-4 text-center">I&#39;m Cherise Stanley</p>
+        <p className="text-center">Cherise Stanley&#39;s bio here</p>
       </div>
     )
   }
