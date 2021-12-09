e9233d5f2321b58735418be98d947c1017b24660
Cherise Stanley <61471777+catstacks@users.noreply.github.com>
2021-12-09 15:23:41 +0000

    Create simple blog

---
+++ pages/bio.js
@@ -0,0 +1,10 @@
+const Bio = () => {
+    return (
+      <div className="mt-3">
+        <p className="display-4 text-center">I'm Cherise Stanley</p>
+        <p className="text-center">Cherise Stanley's bio here</p>
+      </div>
+    )
+  }
+  
+  export default Bio
